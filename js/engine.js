a0c71e0e73ffc2855a70679fa1c75534b623f540
Nick Boober <nboober@terpmail.umd.edu>
2018-05-17 20:14:45 -0400

    Updated the engine.js file by adding the rock and heart png images to the list of resources to load into the game.

--- js/engine.js
+++ js/engine.js
@@ -177,7 +177,9 @@ var Engine = (function(global) {
         "images/char-cat-girl.png",
         "images/char-horn-girl.png",
         "images/char-pink-girl.png",
-        "images/char-princess-girl.png"
+        "images/char-princess-girl.png",
+        "images/Heart.png",
+        "images/Rock.png"
     ]);
     Resources.onReady(init);
 
